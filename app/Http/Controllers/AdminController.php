484e1fbbeadad9f7f87485d3ded203e9e2e73fd8
iam-neo <nirmalrokamagar54@gmail.com>
2023-06-16 14:17:07 +0545

    created blade file for admin and updated admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function AdminDashboard(){
+
+        return view('admin.admin_dashboard');
+
+    }// End method 
 }
